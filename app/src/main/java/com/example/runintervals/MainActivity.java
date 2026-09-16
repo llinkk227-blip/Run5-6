f54de4a3bd94b841c965e014e0e70154c90566fe
llinkk227-blip <llinkk227@gmail.com>
2026-09-17 00:49:54 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/runintervals/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.runintervals;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("RunIntervals");
+        text.setTextSize(24);
+
+        setContentView(text);
+    }
+}
